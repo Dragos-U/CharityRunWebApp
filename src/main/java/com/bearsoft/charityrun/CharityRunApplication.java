1268a77fad1f2b5e39cd677800ab6e0caf467b9c
Dragos Ursu <dragos.ursu15@gmail.com>
2023-11-06 08:40:47 +0200

    project initialization

---
+++ src/main/java/com/bearsoft/charityrun/CharityRunApplication.java
@@ -0,0 +1,13 @@
+package com.bearsoft.charityrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bearsoft/charityrun/CharityRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearsoft.charityrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
